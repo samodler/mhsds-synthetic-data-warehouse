956627b32b4ed44a5d7b38b602e39804f3c8f312
samodler <odlersam@gmail.com>
2026-07-23 12:54:56 +0100

    Load teams, patients, referrals, and care_contacts into MHSDS_DW via BULK INSERT

--- sql/03_insert_synthetic_data.sql
+++ sql/03_insert_synthetic_data.sql
@@ -12,4 +12,79 @@ WITH (
 );
 GO
 
-SELECT * FROM dbo.services
+-- SELECT * FROM dbo.services
+
+BULK INSERT dbo.teams
+FROM '/var/opt/mssql/import/teams.csv'
+WITH(
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0d0a',
+    TABLOCK
+);
+GO
+
+-- SELECT * FROM dbo.teams
+
+BULK INSERT dbo.patients
+FROM '/var/opt/mssql/import/patients.csv'
+WITH(
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0d0a',
+    TABLOCK
+);
+GO
+
+
+/* SELECT 
+    COUNT(*) AS total_patients,
+    SUM(CASE WHEN postcode IS NULL THEN 1 ELSE 0 END) AS missing_postcode,
+    SUM(CASE WHEN gender_code IS NULL THEN 1 ELSE 0 END) as missing_gender_code
+FROM dbo.patients */
+
+BULK INSERT dbo.referrals
+FROM '/var/opt/mssql/import/referrals.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0d0a',
+    TABLOCK
+);
+GO
+
+/* SELECT
+    COUNT(*) as total_referrals,
+    SUM(CASE WHEN team_id IS NULL THEN 1 ELSE 0 END) as missing_team_id,
+    SUM(CASE WHEN discharge_date < referral_date THEN 1 ELSE 0 END) as discharge_before_referral
+FROM dbo.referrals */
+
+/* SELECT 
+    team_id,
+    COUNT(referral_id) AS referrals_per_team
+FROM dbo.referrals
+GROUP BY team_id */
+
+BULK INSERT dbo.care_contacts
+FROM '/var/opt/mssql/import/care_contacts.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0d0a',
+    TABLOCK
+);
+GO
+
+SELECT
+    COUNT(*) as total_contacts,
+    -- cc.referral_id,
+    -- cc.contact_date,
+    -- r.referral_date
+    SUM(CASE WHEN cc.contact_date < r.referral_date THEN 1 ELSE 0 END) AS contacts_before_referral
+FROM dbo.care_contacts AS cc
+JOIN dbo.referrals AS r
+ON cc.referral_id = r.referral_id
